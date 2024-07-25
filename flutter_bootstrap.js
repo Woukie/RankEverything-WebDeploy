35091a011884256f178b04321863aa50ba0c0d7f
Woukie <lucaseldon@live.com>
2024-07-25 00:34:55 +0000

    Bot deploy

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1358582366"
+    serviceWorkerVersion: "3693255963"
   }
 });
